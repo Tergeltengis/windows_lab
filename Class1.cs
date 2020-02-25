905e3cbde465a27a168789cdfa8e1cf1ae46ba5a
Tergeltengis <bayrsaihan_tergeltengis@yahoo.com>
2020-02-25 12:57:36 +0800

    first commit

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Lab1
+{
+    public class User
+    {
+        public string name { get; set; }
+
+        private int _phone;
+
+        public int getPhone { get { return _phone;  } set { _phone = value; } }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Task1 myProgram = new Task1();
+            myProgram.doSomething();
+            Console.WriteLine("hello my friend");
+        }
+
+        public void doSomething()
+        {
+            User employee = new User();
+            employee.name = "Tergeltengis";;
+            employee.getPhone = 91625500;
+            Console.WriteLine("Ner: " + employee.name + " Utas: " + employee.getPhone);
+        }
+    }
+}
